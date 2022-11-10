f0b70ce9e60ee2e45178774788f88db7cc02ef5b
YoungJun Park <youngjun108059@gmail.com>
2022-11-10 23:30:13 +0900

    fix Error

--- todo/urls.py
+++ todo/urls.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('admin/',admin.site.urls),
-]
-
-urlpatterns=[
-    path('',views.todo_list,name="todo_list"),
-]
-
 urlpatterns = [
-    path("", views.todo_list,name='todo_list'),
-    path('<int:pk>/',views.todo_detail,name='todo-detail')
+    path('admin/', admin.site.urls),
+    path('', views.todo_list, name="todo_list"),
+    path('<int:pk>/', views.todo_detail, name='todo-detail')
 ]
