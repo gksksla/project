1e0d7e96b107cf46875b1b3f8424d4174f4aee73
gksksla <khjtero@gmail.com>
2022-11-13 16:52:06 +0900

    first commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,16 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('admin/',admin.site.urls),
-]
-
-urlpatterns=[
-    path('',views.todo_list,name="todo_list"),
-]
 
 urlpatterns = [
     path("", views.todo_list,name='todo_list'),
-    path('<int:pk>/',views.todo_detail,name='todo-detail')
+    path('<int:pk>/',views.todo_detail,name='todo_detail'),
+    path('post/',views.todo_post, name='todo_post'),
+    path('<int:pk>/edit/',views.todo_edit,name='todo_edit'),
+    path('done/',views.done_list,name='done_list'),
+    path('done/<int:pk>/',views.todo_done, name='todo_done'),
 ]
